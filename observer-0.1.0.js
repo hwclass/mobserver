471b0ca454382e97ddff1ab1d9442a9de8865338
hwclass <harderworking.class@gmail.com>
2014-09-01 22:50:25 +0300

    Added the versioned js file.

---
+++ observer-0.1.0.js
@@ -0,0 +1,19 @@
+/*!
+ * observer.js - A UX Tracking Infromation Generator
+ *
+ * Copyright (c) 2014 Barış Güler
+ * http://hwclass.github.io
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * http://docs.jquery.com/Plugins/Authoring
+ * jQuery authoring guidelines
+ *
+ * Launch  : July 2014
+ * Version : 0.1.0
+ * Released: July 29th, 2014
+ *
+ *
+ * makes an element initialize for defined events with their names
+ */
